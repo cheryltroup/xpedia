56221b79e32ed07cc9712085c583453a839fa710
cheyltroup <cheryltroup@gmail.com>
2015-10-31 19:34:41 -0700

    complete CRUD 2

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -8,11 +8,39 @@ class WikisController < ApplicationController
   end
 
   def new
+    @wiki = Wiki.new
   end
+
+ def create
+     @wiki = Wiki.new(params.require(:wiki).permit(:title, :body))
+     if @wiki.save
+       flash[:notice] = "Wiki was saved."
+       redirect_to @wiki
+     else
+       flash[:error] = "There was an error saving the wiki. Please try again."
+       render :new
+     end
+   end
 
   def edit
+    @wiki = Wiki.find(params[:id])
   end
 
+  def update
+     @wiki = Wiki.find(params[:id])
+     if @wiki.update_attributes(params.require(:wiki).permit(:title, :body))
+       flash[:notice] = "Wiki was updated."
+       redirect_to @wiki
+     else
+       flash[:error] = "There was an error saving the wiki. Please try again."
+       render :edit
+     end
+   end
+
   def destroy
+    @wiki = Wiki.find(params[:id])
+    @wiki.destroy
+    flash[:notice] = "Wiki was successfully deleted."
+    redirect_to @wiki
   end
 end
