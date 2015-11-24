b29a02feb2a09f1406df8fcefac18d091c38585e
Brittany Martin <brittany.jill.martin@gmail.com>
2015-11-24 07:01:03 -0500

    Refactored the index, create and destroy methods

--- app/controllers/collaborators_controller.rb
+++ app/controllers/collaborators_controller.rb
@@ -5,7 +5,7 @@ class CollaboratorsController < ApplicationController
     @users = User.all
     @wiki = Wiki.find(params[:wiki_id])
     # get IDs of all collaborators for the current wiki
-    collaborators_ids = @wiki.collaborators(:select => :id).collect(&:id)
+    collaborators_ids = @wiki.collaborators(:select => :user_id).collect(&:user_id)
     # Users that match collaborator IDs
     @wiki_users = User.where(id: collaborators_ids)
     # Users that do not match collaborator IDs
@@ -17,19 +17,17 @@ class CollaboratorsController < ApplicationController
 
   def create
 
-    #@user = current_user
     @wiki = Wiki.find params[:wiki_id]
     @user = User.find params[:user_id]
-    @wiki.collaborators.create user: @user
+    @collaborator = Collaborator.create(user_id: @user.id, wiki_id: @wiki.id)
 
-    #if @collaborator.save
+    if @collaborator.save
       flash[:notice] = "Collaborator was saved."
       redirect_to edit_wiki_path(@wiki)
-    #else
-    # flash[:error] = "Error saving collaborator. Please try again."
-    # render :new
-    #end
-
+    else
+      flash[:error] = "Error saving collaborator. Please try again."
+      render :new
+    end
   end
 
   def show
@@ -37,8 +35,8 @@ class CollaboratorsController < ApplicationController
   end
 
   def destroy
-    @wiki = Wiki.find(params[:wiki_id])
-    @collaborator = @wiki.collaborators.find params[:id]
+    @collaborator = Collaborator.find_by_user_id_and_wiki_id(params[:id],params[:wiki_id])
+
     if @collaborator.destroy
       flash[:notice] = "Collaborator was sucessfully deleted."
       redirect_to edit_wiki_path(@wiki)
