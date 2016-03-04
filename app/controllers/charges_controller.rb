fc2a348a39744bffe841b070096988b32c997a09
cheyltroup <cheryltroup@gmail.com>
2016-03-04 12:27:31 -0800

    remove comments

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,35 +1,34 @@
 class ChargesController < ApplicationController
 
-  before_action :authenticate_user!
+before_action :authenticate_user!
 
- def create
-   
-   customer = Stripe::Customer.create(
-     email: current_user.email,
-     card: params[:stripeToken]
+def create
+  customer = Stripe::Customer.create(
+    email: current_user.email,
+    card: params[:stripeToken]
    )
  
-   charge = Stripe::Charge.create(
-     customer: customer.id, # Note -- this is NOT the user_id in your app
-     amount: amount_for_upgrade,
-     description: "Premium Membership! - #{current_user.email}",
-     currency: 'usd'
+  charge = Stripe::Charge.create(
+    customer: customer.id, 
+    amount: amount_for_upgrade,
+    description: "Premium Membership! - #{current_user.email}",
+    currency: 'usd'
    )
 
-   current_user.update_attribute(:role, 'premium')
+  current_user.update_attribute(:role, 'premium')
    flash[:notice] = "Thanks for upgrading your account!, #{current_user.email}! "
-   redirect_to wikis_path(current_user) # or wherever
+   redirect_to wikis_path(current_user) 
  
-   rescue Stripe::CardError => e
+  rescue Stripe::CardError => e
    flash[:error] = e.message
    redirect_to new_charge_path
- end
+end
 
-  def new
-   @stripe_btn_data = {
-     key: "#{ Rails.configuration.stripe[:publishable_key] }",
-     description: "Premium Membership - #{current_user.name}",
-     amount: amount_for_upgrade
+def new
+  @stripe_btn_data = {
+    key: "#{ Rails.configuration.stripe[:publishable_key] }",
+    description: "Premium Membership - #{current_user.name}",
+    amount: amount_for_upgrade
    }
   end
 
@@ -38,6 +37,4 @@ class ChargesController < ApplicationController
   def amount_for_upgrade
     15_00
   end
-
-  
 end
